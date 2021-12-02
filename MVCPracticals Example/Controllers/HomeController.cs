4044d9e87c7621d05b09ffee930955e9a0f67e3d
paragorkey <paragorkey1@gmail.com>
2021-12-02 13:15:53 +0530

    This is First Example

---
+++ MVCPracticals Example/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPracticals_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Show()
+        {
+            return "This is my string View";
+        }
+
+      
+    }
+}
